e8bae987cdc0f97aa7059750ded0984ca20e4f85
cecil <cecilghimire@gmail.com>
2023-02-01 22:17:46 +0545

    dyanmiic marher acc to the module seleted

--- src/Components/Pages/Portal.jsx
+++ src/Components/Pages/Portal.jsx
@@ -18,6 +18,10 @@ import ReportAnAncident from "../../Sidebar/reportIncident";
 import DataArchieve from "../../Sidebar/dataArchive";
 import Situation from "../../Sidebar/situation";
 import Feedback from "@mui/icons-material/Feedback";
+import { LivePollutionDataAsyncGETThunk } from "../../store/Slices/livedataSlice";
+
+
+
 export const Portal = () => {
   const dispatch = useDispatch();
   var [jsonLalitpurMetro, setJsonLalitpurMetro] = useState("");
@@ -26,14 +30,26 @@ export const Portal = () => {
   const slidebarState = useSelector((state) => {
     return state.slidebar.slidebarState;
   });
+  
+//get dashboard
+
   useEffect(() => {
-    if (postStatus === "idle") {
+    if (postStatus === "idle" && component=="Dashboard") {
+    
       dispatch(disasterAsyncGETThunk());
+    }
+  }, [postStatus, dispatch]);
+  //realtime
+  useEffect(() => {
+    if (postStatus === "idle" && component=="RealTime") {
+      dispatch(LivePollutionDataAsyncGETThunk());
     }
   }, [postStatus, dispatch]);
   const component = useSelector((state) => {
     return state.component;
   });
+
+  console.log(component,'rednered compnent')
   const changeComponent = (compName) => {
     switch (compName) {
       case "Dashboard":
@@ -58,6 +74,7 @@ export const Portal = () => {
         return <Dashboard />;
     }
   };
+
   const ComponentToRender = changeComponent(component);
   const metroJSON = async () => {
     let data = await fetch(
@@ -66,29 +83,45 @@ export const Portal = () => {
     let datajson = await data.json();
     setJsonLalitpurMetro(datajson);
   };
+
   const wardJSON = async () => {
     let data = await fetch("http://127.0.0.1:8000/api/v1/spatial/ward/");
     let datajson = await data.json();
     setJsonWard(datajson);
   };
+
   const changeSlidebarState = () => {
     dispatch(slidebarAction.changeSlidebarState());
   };
+
   useEffect(() => {
     metroJSON();
     wardJSON();
   }, []);
-  const data = useSelector((state) => state.disaster.data);
+  
+ //disaster selector
+  const datadisaster = useSelector((state) => state.disaster.data);
+//
+const realtimedatawater = useSelector((state) => state.live.water);
+const realtimepollution =useSelector((state) => state.live.pollution);
+console.log('realtimewatre',realtimedatawater)
+
+console.log('realtimepollutio',realtimepollution)
+
+console.log('data',realtimedatawater)
+
   const position = [27.67571580617923, 85.3183283194577];
   const scrollWheelZoom = true;
   return (
     <Layout>
+
       <div className="flex">
         <div
           className={`${
             slidebarState ? "w-2/4" : "w-0"
           } duration-300 h-[90vh] relative`}
         >
+
           {ComponentToRender}
           <NavigateNextIcon
             style={{
@@ -103,6 +136,7 @@ export const Portal = () => {
               }`}
             onClick={changeSlidebarState}
           />
+
         </div>
         <MapContainer
           center={position}
@@ -139,9 +173,20 @@ export const Portal = () => {
             icon="url(/some/relative/path.png)"
             position="topright"
           />
-          {data.map((event) => (
+          {component=='Dashboard'?datadisaster.map((event) => {
+            console.log('disaster')
+            return <Markers disaster={event} key={event.id} />
+            }):''}
+
+          {component=='RealTime'?realtimedatawater.map((event) => (  
             <Markers disaster={event} key={event.id} />
-          ))}
+
+          )):''}
+          {component=='RealTime'?realtimepollution.map((event) => (
+      
+            <Markers disaster={event} key={event.id} />
+     
+    )):''}
         </MapContainer>
         <SideBar />
       </div>
