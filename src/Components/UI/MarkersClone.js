fe2c099b82b437e5e86c6be6f3a7d763a9be0c29
cecil <cecilghimire@gmail.com>
2023-02-02 19:59:15 +0545

    pop info update

--- src/Components/UI/MarkersClone.js
+++ src/Components/UI/MarkersClone.js
@@ -78,7 +78,7 @@ function MarkersClone({ disaster: event }) {
           icon={stationIcon}
         >
           <Popup>
-            <div class="">
+            <div class=" p-2">
               <div class="bg-indigo-700 text-white text-md border-gray-200 border-t-2 pt-3 ">
                 <center>
                   <h1>
@@ -90,8 +90,8 @@ function MarkersClone({ disaster: event }) {
               </div>
 
               {event.tags ? (
-                <div class="text-gray-700 text-xs mt-3">
-                   {event.tags[0].name}   {event.tags[0].description}
+                <div class="text-gray-700 text-xs mt-3 ">
+                  {event.tags[0].name} {event.tags[0].description}
                 </div>
               ) : (
                 ""
@@ -101,27 +101,49 @@ function MarkersClone({ disaster: event }) {
               </div>
               <div class="text-black-900 text-sm mt-3">
                 <span className="text-xs text-gray-400">
-                  Data source-{event.dataSource}
+                  Datasource- {event.dataSource}
                 </span>
               </div>
-              <span className="font-normal ml-5 pt-1 text-sm border-t-2 ">
-                  <div className="font-semibold ">{event.status}</div>
-                  <div>
-                    <div className="text-xs text-gray-500  ">
-                      <div className="flex items-center px-2">
-                        <span className="pl-1">{event.waterLevelOn}</span>
-                      </div>
-                    </div>
+              <div className=" border-t-2">
+                <span className="font-normal pt-1 text-sm  ">
+                  {/* <div className="font-semibold text-xs ">{event.status}</div> */}
+                  <div className="text-xs text-gray-500  ">
+                    {event.waterLevelOn ? (
+                      <>
+                       <span className="">Water Level : {event.waterLevel}</span><br/>
+                       <span className="">Warning Level: {event.warningLevel}</span>
+                        <p className="">Updated on :</p>
+                        <span className="">{event.waterLevelOn}</span>
+                      </>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 </span>
-              <div className="text-md font-medium   ">
-                <div className="text-red-700 text-sm   border-indigo-300 border-r-2 pr-4">
-                  <span className="px-2">
-                    {event.images ? <img src={event.images[0]}></img> : ""}
-                    
-                  </span>
-                </div>
               </div>
+
+              {event.images ? (
+                <div className="text-md font-medium   ">
+                  <div className="text-red-700 text-sm   border-indigo-300 border-r-2 pr-4">
+                    <span className="px-2">
+                      <img src={event.images[0]}></img>
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+              {event.image ? (
+                <div className="text-md font-medium   ">
+                  <div className="text-red-700 text-sm   border-indigo-300 border-r-2 pr-4">
+                    <span className="px-2">
+                      <img src={event.image}></img>
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </Popup>
         </Marker>
