0b8357501b8981c55feffc0b045b81e0bf5e03bc
cecil <cecilghimire@gmail.com>
2023-02-06 21:54:15 +0545

    added grid in manage panel

--- src/Components/Pages/AboutUs.jsx
+++ src/Components/Pages/AboutUs.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import Layout from "../Layout/Layout";
-import { MapContainer } from "react-leaflet";
+import { LayersControl, MapContainer, Marker, TileLayer } from "react-leaflet";
 const AboutUs = (props) => {
-
   const scrollWheelZoom = true;
   const position = [27.67571580617923, 85.3183283194577];
-  
+
   return (
     <Layout>
-     <MapContainer
-          center={position}
-          zoom={14}
-          scrollWheelZoom={scrollWheelZoom}
-          className="mt-1 z-10"
-        >
-         <LayersControl position="topright">
-            <LayersControl.BaseLayer name="OSM Streets">
-              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            </LayersControl.BaseLayer>
-            <LayersControl.BaseLayer name="World Imagery">
-              <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
-            </LayersControl.BaseLayer>
-            <LayersControl.BaseLayer checked name="Grey Imagery">
-              <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png" />
-            </LayersControl.BaseLayer>
-            
-          </LayersControl>
-
-        </MapContainer>
+      <div className="grid grid-cols-4">
+      <div className="col-span-2 ">06</div>
+        <div className="col-span-2 ">
+          <MapContainer
+            center={position}
+            zoom={14}
+            scrollWheelZoom={scrollWheelZoom}
+            className="mt-1 z-10"
+          >
+            <LayersControl position="topright">
+              <LayersControl.BaseLayer name="OSM Streets">
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              </LayersControl.BaseLayer>
+              <LayersControl.BaseLayer name="World Imagery">
+                <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
+              </LayersControl.BaseLayer>
+              <LayersControl.BaseLayer checked name="Grey Imagery">
+                <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png" />
+              </LayersControl.BaseLayer>
+            </LayersControl>
+          </MapContainer>
+        </div>
+      </div>
     </Layout>
   );
 };
