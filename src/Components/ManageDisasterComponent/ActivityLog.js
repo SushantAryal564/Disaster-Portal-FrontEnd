bfa195cd545b21df51f8c13741f72af62678848c
sushant <aryalsushant564@gmail.com>
2023-02-14 13:06:33 +0545

    Added validation to the form

--- src/Components/ManageDisasterComponent/ActivityLog.js
+++ src/Components/ManageDisasterComponent/ActivityLog.js
@@ -1,12 +1,31 @@
-import React, { useState, useEffect } from "react";
+import { CloudDone } from "@mui/icons-material";
+import React, { useState, useEffect, useRef } from "react";
 import { Fragment } from "react";
 import { AiFillInfoCircle } from "react-icons/ai";
 import { BiAlarm } from "react-icons/bi";
+import useInput from "../../hooks/formValidationHook";
 
 function ActivityLog({ changeMarkerDataState }) {
+  const refContainerActivity = useRef();
+  const {
+    enteredValue: enteredCreator,
+    valueIsValid: CreatorIsValid,
+    hasError: CreatorHasError,
+    valueHandler: CreatorValueHandler,
+    blurHandler: CreatorBlurHandler,
+    reset: resetCreator,
+  } = useInput((value) => value.trim() != "");
+
   const [WardDisasterData, setWardDisasterData] = useState([]);
   const [DisasterActivityLog, setDisasterActivityLog] = useState("");
   const wardId = localStorage.getItem("WardId");
+  let access_token = localStorage.getItem("access_token");
+  let formIsValid = false;
+  if (CreatorIsValid) {
+    formIsValid = true;
+  }
+  console.log(formIsValid);
+
   const WardActiveDisaster = async () => {
     let data = await fetch(
       `http://127.0.0.1:8000/api/v1/disaster/disasterEventwithoutgeom/?name=&Ward=${wardId}&type=&is_closed=false&startTime__gte=&startTime__gt=&startTime=&startTime__lte=`
@@ -23,12 +42,40 @@ function ActivityLog({ changeMarkerDataState }) {
     setWardDisasterData(wardAllIncident);
     changeMarkerDataState(wardAllIncident);
   };
+  let DisasterId = null;
   const DisasterActivity = async (id) => {
+    DisasterId = id;
     let data = await fetch(
       `http://127.0.0.1:8000/api/v1/response/activity/?disaster__is_closed=&disaster__id=${id}&disaster__Ward=`
     );
     let DisasterActivityLog = await data.json();
     setDisasterActivityLog(DisasterActivityLog);
+  };
+  const ActivitySendToBackend = async (activity, creator, date, id) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", access_token);
+    const response = fetch("http://127.0.0.1:8000/api/v1/response/activity/", {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({
+        disaster: id,
+        action_name: activity,
+        time_of_action: date,
+        logCreator: creator,
+      }),
+    });
+  };
+  const ActivityFormSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(refContainerActivity.current.value, enteredCreator, DisasterId);
+    ActivitySendToBackend(
+      refContainerActivity.current.value,
+      enteredCreator,
+      DisasterId
+    );
+    refContainerActivity.current.value = "";
+    resetCreator();
   };
   useEffect(() => {
     WardActiveDisaster(wardId);
@@ -100,7 +147,10 @@ function ActivityLog({ changeMarkerDataState }) {
         <div className="col-span-4">
           <div>{ActivityLogLayout}</div>
           <div>
-            <form className="w-full max-w-lg">
+            <form
+              className="w-full max-w-lg"
+              onSubmit={ActivityFormSubmitHandler}
+            >
               <div class="w-full px-3">
                 <label
                   class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -108,12 +158,13 @@ function ActivityLog({ changeMarkerDataState }) {
                 >
                   Response Activity
                 </label>
-                <input
+                <textarea
                   class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-password"
                   type="text"
-                  placeholder="Activity"
-                />
+                  placeholder="Type response Activity Here....."
+                  required
+                  ref={refContainerActivity}
+                ></textarea>
               </div>
               <div class="flex flex-wrap w-full px-3">
                 <div class="w-full md:w-1/2 mb-6 md:mb-0">
@@ -128,6 +179,9 @@ function ActivityLog({ changeMarkerDataState }) {
                     id="grid-first-name"
                     type="text"
                     placeholder="Jane"
+                    onChange={CreatorValueHandler}
+                    onBlur={CreatorBlurHandler}
+                    value={enteredCreator}
                   />
                 </div>
                 <div class="w-full md:w-1/2 px-3">
@@ -150,6 +204,7 @@ function ActivityLog({ changeMarkerDataState }) {
                   <button
                     class="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                     type="button"
+                    disabled={!formIsValid}
                   >
                     Submit
                   </button>
