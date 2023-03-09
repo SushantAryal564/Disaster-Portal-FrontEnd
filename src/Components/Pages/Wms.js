eb4a618a1a89d4e755ed6d9180a065a9c7347ff1
cecil <cecilghimire@gmail.com>
2023-03-09 20:18:51 +0545

    added layer in risk info

---
+++ src/Components/Pages/Wms.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { MapContainer,  TileLayer, useMap,Pane ,WMSTileLayer} from 'react-leaflet';
+
+import L from 'leaflet';
+
+export const Wms = (layer) => {
+  //  var map=useMap()
+  //    var layer=L.tileLayer.wms('http://localhost:8080/geoserver/new/wms', {
+  //   layers: 'new:lalitpurWard',
+  //   format: 'image/png',
+  //   transparent: true,
+  //   version: '1.1.0',
+  //   styles:'ward'
+  //  }).addTo(map);
+  //  layer.addTo(map).bringToFront()
+
+    return (
+    <>
+    <Pane name="myPane" style={{ zIndex: 650 }}>
+    <WMSTileLayer url='http://localhost:8080/geoserver/new/wms' params={{layers: 'new:lalitpurWard',
+    format: 'image/png',
+    transparent: true,
+    version: '1.1.0',
+    styles:'ward'}}></WMSTileLayer>
+    </Pane>
+    </>
+  )
+
+    }
